06450429d1608557eedb729af59063b5fea1c4b9
ga_bin <tgb5808@gmail.com>
2026-08-19 17:57:34 +0900

    redpill day12 과제

--- redpill/day11_p_calculator.c
+++ redpill/day11_p_calculator.c
@@ -1,70 +1,26 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 
-int add(int a, int b)
-{
-    printf("Input two integers: ");
-    scanf("%d %d", &a, &b);
-    return a + b;
-}
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
+int div_(int a, int b) { return a / b; }
 
-int sub(int a, int b)
-{
-    printf("Input two integers: ");
-    scanf("%d %d", &a, &b);
-    return a - b;
-}
+enum { ADD, SUB, MUL, DIV, EXIT };
 
-int mul(int a, int b)
-{
-    printf("Input two integers: ");
-    scanf("%d %d", &a, &b);
-    return a * b;
-}
-
-int div_(int a, int b)
-{
-    printf("Input two integers: ");
-    scanf("%d %d", &a, &b);
-    return a / b;
-}
-
-
-
-int do_exit(int a, int b)
-{
-    exit(0);//함수 즉시 종료
-    return 0;
-}
-
-
-
-
-enum
-{
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    EXIT,
-};
-
-int (*operations[])(int, int) = { add, sub, mul, div_, do_exit };
+int (*operations[])(int, int) = { add, sub, mul, div_ };
 
 int main(void)
 {
     printf("=== Day 11: Function Pointer Array Calculator ===\n\n");
+    int choice, a, b;
 
-    int choice;  //연산 번호를 담을 변수
-
-    while (1)
+    printf("Select (0:Add, 1:Sub, 2:Mul, 3:Div, 4:Exit): ");
+    while (scanf("%d", &choice) == 1 && choice != EXIT)
     {
+        printf("Input two integers: ");
+        scanf("%d %d", &a, &b);
+        printf(">> Result: %d\n\n", operations[choice](a, b));
         printf("Select (0:Add, 1:Sub, 2:Mul, 3:Div, 4:Exit): ");
-        scanf("%d", &choice);
-
-        printf(">> Result: %d\n\n", operations[choice](0, 0));
     }
-
     return 0;
 }

8826fbb10d25f9338f646e36f442f90aa757af05
ga_bin <tgb5808@gmail.com>
2026-08-20 20:07:31 +0900

    day13 수정

--- redpill/day13_memory_pool.c
+++ redpill/day13_memory_pool.c
@@ -28,21 +28,57 @@ typedef struct {
 } MemoryPool;
 
 // 1. 초기화: 모든 메모리를 쪼개서 연결 리스트로 연결
-void pool_init(MemoryPool* pool) {}
+void pool_init(MemoryPool* pool)
+{
+    pool->used_count = 0;
+
+    for (int i = 0; i < POOL_SIZE; i++)
+    {
+        Block* cur = (Block*)&pool->memory_area[i * BLOCK_SIZE];
+
+        if (i == POOL_SIZE - 1)
+            cur->next = NULL;
+        else
+            cur->next = (Block*)&pool->memory_area[(i+1) * BLOCK_SIZE];
+    }
+
+    pool->free_list = (Block*)&pool->memory_area[0];
+}
 
 // 2. 할당: Free List의 헤드(Head)를 떼어줌 (Pop)
-void* pool_alloc(MemoryPool* pool) {}
+void* pool_alloc(MemoryPool* pool)
+{
+    if (pool->free_list == NULL)
+        return NULL;
+
+    Block* block = pool->free_list;
+    pool->free_list = block->next;
+    pool->used_count++;
+
+    return (void*)block;
+}
 
 // 3. 해제: 반환된 블록을 Free List의 헤드에 다시 붙임 (Push)
-void pool_free(MemoryPool* pool, void* ptr) {}
+void pool_free(MemoryPool* pool, void* ptr)
+{
+    Block* block = (Block*)ptr;
+    block->next = pool->free_list;
+    pool->free_list = block;
+    pool->used_count--;
+}
 
 
 
+
+MemoryPool my_pool;
 
 int main(void){
 
     //테스트 코드
     pool_init(&my_pool);
+
+    printf("=== Day 13: Fixed-size Memory Pool Allocator ===\n\n");
+    printf("[Init] Memory Pool Initialized (%d blocks of %d bytes)\n", POOL_SIZE, BLOCK_SIZE);
 
     // 3개 할당
     void* p1 = pool_alloc(&my_pool);
@@ -63,7 +99,6 @@ int main(void){
 
 
 
-    //실제 코드
 
     
 
